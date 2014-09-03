82d19d715d5dfe8e6a4be66160b72544462db05c
Jayavelu <jayaveluraj@gmail.com>
2014-09-03 14:13:57 +0530

    Initial Commit

---
+++ Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Jayavelu on 9/3/14.
+//  Copyright (c) 2014 Jayavelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Jayavelu on 9/3/14.
+//  Copyright (c) 2014 Jayavelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
