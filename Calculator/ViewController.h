dd333b33d319055795ad61c7a1c7aedb6472bc28
Jayavelu <jayaveluraj@gmail.com>
2014-09-05 11:22:47 +0530

    “initial commit”

--- Calculator/ViewController.h
+++ Calculator/ViewController.h
@@ -9,5 +9,24 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+{
+    IBOutlet UILabel *screen;
+    
+}
+-(IBAction)selector1:(id)sender;
+-(IBAction)selector2:(id)sender;
+-(IBAction)selector3:(id)sender;
+-(IBAction)selector4:(id)sender;
+-(IBAction)selector5:(id)sender;
+-(IBAction)selector6:(id)sender;
+-(IBAction)selector7:(id)sender;
+-(IBAction)selector8:(id)sender;
+-(IBAction)selector9:(id)sender;
+-(IBAction)selector0:(id)sender;
+-(IBAction)division:(id)sender;
+-(IBAction)multipl:(id)sender;
+-(IBAction)subtraction:(id)sender;
+-(IBAction)addtion:(id)sender;
+-(IBAction)timer:(id)sender;
+-(IBAction)anser:(id)sender;
 @end
--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -8,11 +8,241 @@
 
 #import "ViewController.h"
 
+int method;
+int selectNumber;
+float total;
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+-(IBAction)selector1:(id)sender{
+    selectNumber=selectNumber*10;
+    selectNumber=selectNumber+1;
+    screen.text=[NSString stringWithFormat:@"%i",selectNumber];
+    
+}
+-(IBAction)selector2:(id)sender{
+    selectNumber=selectNumber*10;
+    selectNumber=selectNumber+2;
+    screen.text=[NSString stringWithFormat:@"%i",selectNumber];
+}
+-(IBAction)selector3:(id)sender{
+    selectNumber=selectNumber*10;
+    selectNumber=selectNumber+3;
+    screen.text=[NSString stringWithFormat:@"%i",selectNumber];
+    
+}
+-(IBAction)selector4:(id)sender{
+    selectNumber=selectNumber*10;
+    selectNumber=selectNumber+4;
+    screen.text=[NSString stringWithFormat:@"%i",selectNumber];
+    
+}
+-(IBAction)selector5:(id)sender{
+    selectNumber=selectNumber*10;
+    selectNumber=selectNumber+5;
+    screen.text=[NSString stringWithFormat:@"%i",selectNumber];
+    
+}
+-(IBAction)selector6:(id)sender{
+    selectNumber=selectNumber*10;
+    selectNumber=selectNumber+6;
+    screen.text=[NSString stringWithFormat:@"%i",selectNumber];
+    
+}
+-(IBAction)selector7:(id)sender{
+    selectNumber=selectNumber*10;
+    selectNumber=selectNumber+7;
+    screen.text=[NSString stringWithFormat:@"%i",selectNumber];
+    
+}
+-(IBAction)selector8:(id)sender{
+    selectNumber=selectNumber*10;
+    selectNumber=selectNumber+8;
+    screen.text=[NSString stringWithFormat:@"%i",selectNumber];
+    
+}
+-(IBAction)selector9:(id)sender{
+    selectNumber=selectNumber*10;
+    selectNumber=selectNumber+9;
+    screen.text=[NSString stringWithFormat:@"%i",selectNumber];
+    
+}
+-(IBAction)selector0:(id)sender{
+    selectNumber=selectNumber*10;
+    selectNumber=selectNumber+0;
+    screen.text=[NSString stringWithFormat:@"%i",selectNumber];
+    
+}
+-(IBAction)division:(id)sender{
+  
+    if(total==0)
+    {
+        total=selectNumber;
+    }
+    else{
+        switch(method)
+        {
+            case 1:
+                total=total/selectNumber;
+                break;
+            case 2:
+                total=total*selectNumber;
+                break;
+                
+            case 3:
+                total=total-selectNumber;
+                break;
+            case 4:
+                total=total+selectNumber;
+                break;
+            default:
+                break;
+                
+        }
+        
+    }
+    method=1;
+    selectNumber=0;
+    
+    
+}
+-(IBAction)multipl:(id)sender{
+    
+    if(total==0)
+    {
+        total=selectNumber;
+    }
+    else{
+        switch(method)
+        {
+            case 1:
+                total=total/selectNumber;
+                break;
+            case 2:
+                total=total*selectNumber;
+                break;
+                
+            case 3:
+                total=total-selectNumber;
+                break;
+            case 4:
+                total=total+selectNumber;
+                break;
+            default:
+                break;
+                
+        }
+        
+    }
+    method=2;
+    selectNumber=0;
+    
+    
+}
+-(IBAction)subtraction:(id)sender{
+    if(total==0)
+    {
+        total=selectNumber;
+    }
+    else{
+        switch(method)
+        {
+            case 1:
+                total=total/selectNumber;
+                break;
+            case 2:
+                total=total*selectNumber;
+                break;
+                
+            case 3:
+                total=total-selectNumber;
+                break;
+            case 4:
+                total=total+selectNumber;
+                break;
+            default:
+                break;
+                
+        }
+        
+    }
+    method=3;
+    selectNumber=0;
+    }
+-(IBAction)addtion:(id)sender{
+    if(total==0)
+    {
+        total=selectNumber;
+    }
+    else{
+        switch(method)
+        {
+            case 1:
+                total=total/selectNumber;
+                break;
+            case 2:
+                total=total*selectNumber;
+                break;
+                
+            case 3:
+                total=total-selectNumber;
+                break;
+            case 4:
+                total=total+selectNumber;
+                break;
+            default:
+                break;
+                
+        }
+        
+    }
+    method=4;
+    selectNumber=0;
+  
+    
+}
+-(IBAction)timer:(id)sender{
+    method=0;
+    selectNumber=0;
+    total=0;
+    screen.text=[NSString stringWithFormat:@"0"];
+    
+}
+-(IBAction)anser:(id)sender{
+    if(total==0)
+    {
+        total=selectNumber;
+    }
+    else{
+        switch(method)
+        {
+            case 1:
+                total=total/selectNumber;
+                break;
+            case 2:
+                total=total*selectNumber;
+                break;
+                
+            case 3:
+                total=total-selectNumber;
+                break;
+            case 4:
+                total=total+selectNumber;
+                break;
+            default:
+                break;
+                
+        }
+        
+    }
+    method=0;
+    selectNumber=0;
+    screen.text=[NSString stringWithFormat:@"%.2f",total];
+    //total=0;
+    
+}
 
 - (void)viewDidLoad
 {
